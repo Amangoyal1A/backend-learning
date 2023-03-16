143c914695f04e66ddf2dd72e6cd78c3abb51367
Aman goyal <amangoyal@Amans-MacBook-Air.local>
2023-03-16 15:07:44 +0530

    refactored code

--- Controllers/authRouter.js
+++ Controllers/authRouter.js
@@ -1,11 +1,11 @@
 const express = require("express");
-require('dotenv').config();
+require("dotenv").config();
 const app = express();
 const userModel = require("../models/userModel");
 const authRouter = express.Router();
 const path = require("path");
-const bcrypt = require("bcrypt")
-const jwt= require('jsonwebtoken')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 //base router in main.js
 authRouter
   .route("/signup")
@@ -42,49 +42,45 @@ async function postSignup(req, res) {
 }
 
 async function loginUser(req, res) {
-    try {
-      const { email, password } = req.body;
-  
-      const user = await userModel.findOne({ email });
-  
-      if (user) {
-         const isMatch = await bcrypt.compare(password, user.password);
- 
+  try {
+    const { email, password } = req.body;
 
+    const user = await userModel.findOne({ email });
+    if (user) {
+      const isMatch = await bcrypt.compare(password, user.password);
 
-        if (isMatch) {
-       //JWT 
-        let payload = user['_id']
-        let JWT_key = "eui34kh54"
+      if (isMatch) {
+        //JWT
+        let payload = user["_id"];
+        let JWT_key = "eui34kh54";
 
-        let jwttoken = jwt.sign({payload:payload},JWT_key)
-            res.cookie("isLoggedIn", jwttoken, {
-                httpOnly: true,
-              });  
-          return res.status(200).json({
-            message: 'User login successful',
-            user: user
-          });
-        } else {
-          return res.status(401).json({
-            message: 'Invalid email or password'
-          });
-        }
+        let jwttoken = jwt.sign({ payload: payload }, JWT_key);
+        res.cookie("isLoggedIn", jwttoken, {
+          httpOnly: true,
+        });
+        return res.status(200).json({
+          message: "User login successful",
+          user: user,
+        });
       } else {
-        return res.status(404).json({
-          message: 'User not found'
+        return res.status(401).json({
+          message: "Invalid email or password",
         });
       }
-    } catch (err) {
-      console.error(err);
-      return res.status(500).json({
-        message: 'An error occurred while logging in'
+    } else {
+      return res.status(404).json({
+        message: "User not found",
       });
     }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      message: "An error occurred while logging in",
+    });
   }
+}
 
-  module.exports = {
-authRouter,
-postSignup
-  }
-  
+module.exports = {
+  authRouter,
+  postSignup,
+};
